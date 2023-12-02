3b4caf0a9ff908b38e843b4ac7bb72ff243cecd7
ECOMAN003 <ekiliheman@gmail.com>
2023-12-02 17:26:40 +0100

    fixed literally everyting

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -45,6 +45,7 @@ class _WeatherPageState extends State<WeatherPage> {
       case 'fog':
         return 'assets/cloud.json';
       case 'rain':
+        return 'assets/rain.json';
       case 'drizzle':
       case 'shower rain':
         return 'assets/rain.json';
@@ -68,43 +69,69 @@ class _WeatherPageState extends State<WeatherPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(Icons.location_on,
-            size: 18.0,
-            color: Colors.black26,
-            ),
-            const SizedBox(height: 5.0,),
-
-            //city name
-            Text(_weather?.cityName ?? "loading city...",
-            style: GoogleFonts.bebasNeue(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.black45
-            ),
-            ),
-            const SizedBox(height: 150.0,),
-
-            //animation
-            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
-
-            const SizedBox(height: 150.0,),
-
-            //temperature
-            Text('${_weather?.temperature.round()}' 'º',
-              style: GoogleFonts.bebasNeue(
-                color: Colors.black54,
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 60.0,
               ),
-            ),
-            //weather condition
-            Text(_weather?.mainCondition ?? "")
-          ],
+              const Icon(
+                Icons.location_on,
+                size: 30.0,
+                color: Colors.black26,
+              ),
+
+              //city name
+              Text(
+                _weather?.cityName ?? "loading city...",
+                style: GoogleFonts.bebasNeue(
+                    fontSize: 60.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black45),
+              ),
+
+              //animation
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 100.0),
+                  child: Lottie.asset(
+                      getWeatherAnimation(_weather?.mainCondition)),
+                ),
+              ),
+
+              //temperature and weather condition
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '${_weather?.temperature.round()}' '°',
+                    style: GoogleFonts.bebasNeue(
+                      color: Colors.black54,
+                      fontSize: 60.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 30.0,
+                  ),
+                  //weather condition
+                  Text(
+                    _weather?.mainCondition ?? "",
+                    style: GoogleFonts.bebasNeue(
+                      color: Colors.black26,
+                      fontSize: 60.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+
+              const SizedBox(height: 40.0,)
+            ],
+          ),
         ),
       ),
     );
