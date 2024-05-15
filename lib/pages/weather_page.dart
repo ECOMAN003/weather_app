291b39c44dde8a982a6c0ec06f351de61bd5b18c
ECOMAN003 <ekiliheman@gmail.com>
2024-05-15 09:00:30 +0100

    fixed weather page

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -35,7 +35,7 @@ class _WeatherPageState extends State<WeatherPage> {
   //weather animations
   String getWeatherAnimation(String? mainCondition) {
     if (mainCondition == null) return 'assets/sunny.json'; //default animation
-   
+
     switch (mainCondition) {
       case 'Clouds':
       case 'Mist':
@@ -70,43 +70,46 @@ class _WeatherPageState extends State<WeatherPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 60.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(
-                Icons.location_on,
-                size: 60.0,
-                color: Colors.black26,
-              ),
+        child: Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 60.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.location_on,
+                  size: 60.0,
+                  color: Colors.black26,
+                ),
 
-              // const SizedBox(width: 10.0),
+                // const SizedBox(width: 10.0),
 
-              //city name
-              Text(
-                _weather?.cityName ?? "loading city...",
-                style: GoogleFonts.bebasNeue(
-                    fontSize: 60.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black45),
-              ),
-                ],
-              ),
+                //city name
+                Text(
+                  _weather?.cityName ?? "loading city...",
+                  style: GoogleFonts.bebasNeue(
+                      fontSize: 60.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black45),
+                ),
+              ],
+            ),
 
             //animation
             Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
 
-            const SizedBox(height: 150.0,),
+            const SizedBox(
+              height: 150.0,
+            ),
 
             //temperature
-            Text('${_weather?.temperature.round()}' 'º',
+            Text(
+              '${_weather?.temperature.round()}' 'º',
               style: GoogleFonts.bebasNeue(
                 color: Colors.black54,
                 fontSize: 40.0,
@@ -118,6 +121,7 @@ class _WeatherPageState extends State<WeatherPage> {
           ],
         ),
       ),
+    )
     );
   }
 }
